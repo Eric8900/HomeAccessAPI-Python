300915fc014341f86d1f4f0dd45b9f7c71f77229
Eric8900 <ericchen890@gmail.com>
2024-03-14 19:09:10 -0500

    CORs

--- hacAPI.py
+++ hacAPI.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def login(username, password):
     session = HTMLSession()
